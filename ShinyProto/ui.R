b7e8880b0a3d703937bc3e705b4a42e713bc2915
daviesbj <daviesbj@bp.com>
2015-04-12 00:52:29 +0100

    Tidied up the help text

--- ShinyProto/ui.R
+++ ShinyProto/ui.R
@@ -4,12 +4,12 @@ shinyUI(fluidPage(
 
   h4( "Instructions" ),
 
-  helpText( "You might have to wait a few moments for data files to get loaded.",
+  helpText( "Wait a few moments for data files to load.",
             "Once you see the message \"WAITING FOR A SPACE ...\", it's ready." ),
   helpText( "Start typing a sentence.",
             "Every time you type one or more spaces, the app will suggest the next word." ),
-  helpText( "If you like it, hit the ACCEPT button. Otherwise, just keep typing." ),
-  helpText( "Use the RESET button to clear the input window" ),
+  helpText( "If you like it, hit ACCEPT. Otherwise, just keep typing.",
+            "Use RESET to clear the input window." ),
 
   hr(),
 
