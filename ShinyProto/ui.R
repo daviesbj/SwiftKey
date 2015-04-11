693f256b872c757b880b00a0afd69b229b485e96
daviesbj <daviesbj@bp.com>
2015-04-11 11:59:03 +0100

    Added "Accept" button

--- ShinyProto/ui.R
+++ ShinyProto/ui.R
@@ -8,12 +8,16 @@ shinyUI(fluidPage(
   hr(),
 
   textInput("userSentence", label = h4("Sentence input"), value = ""),
+  tags$head( tags$style(type="text/css", "#userSentence {width: 600px}" )),
 
   hr(),
 
-  h4( "Suggested next word" )
+  h4( "Suggested next word" ),
 
-  fluidRow(column(3, verbatimTextOutput( "nextWord" )))
+  fluidRow(column(3, verbatimTextOutput( "nextWord" ))),
+
+  actionButton( "acceptSuggestion", "Accept" )
+
 
 ))
 
